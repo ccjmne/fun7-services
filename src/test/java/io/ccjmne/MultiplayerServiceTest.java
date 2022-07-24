874d37d1ad1721780f25e72fa3fc7ece7e3888e3
ccjmne <ccjmne@gmail.com>
2022-07-24 19:34:25 +0200

    Implement tests for MultiplayerService

---
+++ src/test/java/io/ccjmne/MultiplayerServiceTest.java
@@ -0,0 +1,52 @@
+package io.ccjmne;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import javax.inject.Inject;
+
+import org.bson.types.ObjectId;
+import org.junit.jupiter.api.Test;
+
+import com.neovisionaries.i18n.CountryCode;
+
+import io.ccjmne.check_services.services.MultiplayerService;
+import io.ccjmne.config.MultiplayerConfig;
+import io.ccjmne.users.User;
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class MultiplayerServiceTest {
+
+  @Inject
+  MultiplayerService multiplayer;
+
+  @Inject
+  MultiplayerConfig config;
+
+  @Test
+  public void multiplayer_ok() {
+    assertTrue(multiplayer.isAvailableTo(multiplayerUser()));
+  }
+
+  @Test
+  public void multiplayer_otherRegion() {
+    final User user = multiplayerUser();
+    user.country = config.country() == CountryCode.US ? CountryCode.NZ : CountryCode.US;
+
+    assertFalse(multiplayer.isAvailableTo(user));
+  }
+
+  @Test
+  public void multiplayer_novice() {
+    final User user = multiplayerUser();
+    user.apiCalls--;
+
+    assertFalse(multiplayer.isAvailableTo(user));
+  }
+
+  private User multiplayerUser() {
+    return new User(new ObjectId("123456789012345678901234"), config.country(), config.minCheckins());
+  }
+
+}
